dc31d82b0e52923eac07360b0f6b5091b9e4d6cf
felipe <enggazolla@gmail.com>
2024-09-27 11:16:24 -0300

    OnPress exercise details

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -2,6 +2,8 @@ import { ExerciseCard } from '@components/ExerciseCard'
 import { Group } from '@components/Group'
 import { HomeHeader } from '@components/HomeHeader'
 import { Heading, HStack, Text, VStack } from '@gluestack-ui/themed'
+import { useNavigation } from '@react-navigation/native'
+import type { AppNavigatorRoutesProps } from '@routes/app.routes'
 import { useState } from 'react'
 import { FlatList } from 'react-native'
 
@@ -26,6 +28,12 @@ export function Home() {
     'Supino Inclinado',
   ])
   const [groupSelected, setGroupSelected] = useState('peitoral')
+
+  const navigation = useNavigation<AppNavigatorRoutesProps>()
+
+  function handleOpenExerciseDetails() {
+    navigation.navigate('exercise')
+  }
 
   return (
     <VStack flex={1}>
@@ -60,7 +68,7 @@ export function Home() {
         <FlatList
           data={exercises}
           keyExtractor={item => item}
-          renderItem={() => <ExerciseCard />}
+          renderItem={() => <ExerciseCard onPress={handleOpenExerciseDetails}/>}
           showsVerticalScrollIndicator={false}
           contentContainerStyle={{paddingBottom: 20}}
 
