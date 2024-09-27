b64ae5cc840af6021c6363bb552db1a9c68e3e4a
felipe <enggazolla@gmail.com>
2024-09-27 09:22:35 -0300

    changed title in ScreenHeader

--- src/screens/History.tsx
+++ src/screens/History.tsx
@@ -18,7 +18,7 @@ export function History() {
 
   return (
     <VStack flex={1}>
-      <ScreenHeader title="Histórico" />
+      <ScreenHeader title="Histórico de Exercícios" />
 
       <SectionList
         sections={exercises}
